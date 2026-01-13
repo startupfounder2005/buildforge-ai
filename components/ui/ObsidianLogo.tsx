4882070f8fb5bb71f87052fa7c226a62ecf1120b
startupfounder08 <startupfounder08@gmail.com>
2026-01-13 19:34:43 +0000

    Visuals: Extend Sledgehammer Handle length

--- components/ui/ObsidianLogo.tsx
+++ components/ui/ObsidianLogo.tsx
@@ -8,17 +8,17 @@ export const ObsidianLogo = ({ className }: { className?: string }) => {
             xmlns="http://www.w3.org/2000/svg"
             className={className}
         >
-            {/* Group rotated 45 degrees around center */}
-            <g transform="rotate(45 50 50)">
-                {/* Handle (Purple) - Vertical before rotation */}
-                <rect x="42" y="35" width="16" height="60" rx="2" fill="#7C3AED" />
+            {/* Group rotated 45 degrees around center, scaled down to fit longer handle */}
+            <g transform="rotate(45 50 50) scale(0.85 0.85) translate(10 10)">
+                {/* Handle (Purple) - Significantly longer */}
+                <rect x="42" y="35" width="16" height="85" rx="2" fill="#7C3AED" />
 
-                {/* Head (Purple) - Horizontal block before rotation */}
+                {/* Head (Purple) */}
                 <rect x="15" y="15" width="70" height="30" rx="4" fill="#7C3AED" />
 
-                {/* Black Lightning Crack - Running vertical through head and into handle */}
+                {/* Black Lightning Crack - Extended down the handle */}
                 <path
-                    d="M50 15 L46 25 L54 35 L46 45 L52 55 L48 75"
+                    d="M50 15 L46 25 L54 35 L46 45 L52 55 L48 70 L51 90"
                     stroke="black"
                     strokeWidth="4"
                     strokeLinecap="round"
